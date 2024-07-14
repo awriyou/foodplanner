e19b46057f6653167cde256e91301194bb50bef5
awriyou <febriaryo@gmail.com>
2024-07-14 15:23:50 +0700

    add : LIKEDRECIPE API Implemented

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const useFetch = () => {
-  const apiUrl = `http://192.168.1.8:3000/`;
+  const apiUrl = `http://192.168.229.1:3000/`;
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
--- screens/LikedRecipesScreen.js
+++ screens/LikedRecipesScreen.js
@@ -1,14 +1,68 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import recipeData from '../store/data'
-import { SIZES } from "../constant/styles";
-import RecipeCardView from "../components/Search/RecipeCardView";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import recipeData from '../store/data';
+import { SIZES } from '../constant/styles';
+import RecipeCardView from '../components/Search/RecipeCardView';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import useFetch from '../hook/useFetch';
+import MustLogin from '../components/Auth/MustLogin';
+import axios from 'axios';
 
-const LikedRecipesScreen = () => {
+const LikedRecipesScreen = ({navigation}) => {
+  const { apiUrl } = useFetch();
+  
+  const [isLoading, setIsLoading] = useState(false);
+  const [likedRecipes, setLikedRecipes] = useState([]);
+  const [userData, setUserData] = useState(null);
+  const [userLogin, setUserLogin] = useState(false);
+
+
+  async function checkExistingUser() {
+    const id = await AsyncStorage.getItem('id');
+    const useId = `user${JSON.parse(id)}`;
+
+    try {
+      const currentUser = await AsyncStorage.getItem(useId);
+
+      if (currentUser !== null) {
+        const parsedData = JSON.parse(currentUser);
+        setUserData(parsedData);
+        setUserLogin(true);
+      } else {
+        navigation.navigate('Auth');
+      }
+    } catch (error) {
+      console.log('Error retrieving the data: ', error);
+    }
+  }
+
+  async function getFavoriteData() {
+    const id = await AsyncStorage.getItem('id');
+    const parsedData = JSON.parse(id);
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `${apiUrl}api/users/favorite/${parsedData}`
+      );
+      setLikedRecipes(response.data);
+      // console.log(likedRecipes)
+    } catch (error) {
+      console.log('Error : ', error);
+    }finally{
+      setIsLoading(false)
+    }
+  }
+
+    useEffect(() => {
+      checkExistingUser();
+      getFavoriteData();
+    }, []);
+
   return (
     <View style={styles.containerList}>
+    {!userLogin ? <MustLogin navigation={navigation}/> : <View></View>}
       <FlatList
-        data={recipeData}
+        data={likedRecipes}
         numColumns={2}
         // contentContainerStyle={{ columnGap: SIZES.medium }}
         renderItem={({ item }) => <RecipeCardView item={item}/>}
