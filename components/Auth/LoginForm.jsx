bba35d1fe7b2437aede2bb8edece1ab526ff25c7
awriyou <febriaryo@gmail.com>
2024-09-09 01:37:20 +0700

    feat : implement api forgotpassword in login form

--- components/Auth/LoginForm.jsx
+++ components/Auth/LoginForm.jsx
@@ -26,203 +26,239 @@ const validationSchema = Yup.object({
     .required('Your password'),
 });
 
-const LoginForm = ({ navigation }) => {
-  // const apiUrl = process.env.EXPO_PUBLIC_API_URL;
-  
-  const { apiUrl } = useFetch();
-  const [obsecureText, setObsecureText] = useState(true);
-  const [loader, setLoader] = useState(false);
+const LoginForm = ({ navigation, openForgotPasswordModal }) => {
+    // const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
-  const login = async (values) => {
-    setLoader(true);
-    try {
-      const endpoint = `${apiUrl}api/users/login`;
-      const response = await axios.post(endpoint, values);
-      if (response.status === 200) {
-        const responseData = response.data;
-        await AsyncStorage.setItem(
-          `user${responseData._id}`,
-          JSON.stringify(responseData)
-        );
-        await AsyncStorage.setItem('id', JSON.stringify(responseData._id));
-        navigation.replace('Tabs');
-      } else {
-        Alert.alert('Error Logging in', 'Please Provide valid credentials', [
-          {
-            text: 'Try again',
-            onPress: () => {},
-          },
-        ]);
-      }
-    } catch (error) {
-      Alert.alert(
-        'Error',
-        'Oops, Error logging in. try login again with correct credentials',
-        [
-          {
-            text: 'Try again',
-            onPress: () => {},
-          },
-        ]
-      );
-    } finally {
-      setLoader(false);
-    }
-  };
+    const { apiUrl } = useFetch();
+    const [obsecureText, setObsecureText] = useState(true);
+    const [loader, setLoader] = useState(false);
 
-  return (
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values) => {
-        login(values);
-      }}
-    >
-      {({
-        handleChange,
-        handleSubmit,
-        values,
-        touched,
-        errors,
-        isValid,
-        setFieldTouched,
-      }) => (
-        <View>
-          <View style={styles.inputWrapper}>
-            <TextInput
-              placeholder="Email"
-              allowFontScaling={false}
-              style={styles.input}
-              onFocus={() => {
-                setFieldTouched('email');
-              }}
-              onBlur={() => {
-                setFieldTouched('email', '');
-              }}
-              value={values.email}
-              onChangeText={handleChange('email')}
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-            {touched.email && errors.email && (
-              <Text style={styles.errorMessage}>{errors.email}</Text>
+    const login = async (values) => {
+        setLoader(true);
+        try {
+            const endpoint = `${apiUrl}api/users/login`;
+            const response = await axios.post(endpoint, values);
+            if (response.status === 200) {
+                const responseData = response.data;
+                await AsyncStorage.setItem(
+                    `user${responseData._id}`,
+                    JSON.stringify(responseData)
+                );
+                await AsyncStorage.setItem(
+                    'id',
+                    JSON.stringify(responseData._id)
+                );
+                navigation.replace('Tabs');
+            } else {
+                Alert.alert(
+                    'Error Logging in',
+                    'Please Provide valid credentials',
+                    [
+                        {
+                            text: 'Try again',
+                            onPress: () => {},
+                        },
+                    ]
+                );
+            }
+        } catch (error) {
+            Alert.alert(
+                'Error',
+                'Oops, Error logging in. try login again with correct credentials',
+                [
+                    {
+                        text: 'Try again',
+                        onPress: () => {},
+                    },
+                ]
+            );
+        } finally {
+            setLoader(false);
+        }
+    };
+
+    return (
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+                login(values);
+            }}
+        >
+            {({
+                handleChange,
+                handleSubmit,
+                values,
+                touched,
+                errors,
+                isValid,
+                setFieldTouched,
+            }) => (
+                <View>
+                    <View style={styles.inputWrapper}>
+                        <TextInput
+                            placeholder="Email"
+                            allowFontScaling={false}
+                            style={styles.input}
+                            onFocus={() => {
+                                setFieldTouched('email');
+                            }}
+                            onBlur={() => {
+                                setFieldTouched('email', '');
+                            }}
+                            value={values.email}
+                            onChangeText={handleChange('email')}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                        />
+                        {touched.email && errors.email && (
+                            <Text style={styles.errorMessage}>
+                                {errors.email}
+                            </Text>
+                        )}
+                    </View>
+
+                    <View style={styles.inputWrapper}>
+                        <TextInput
+                            placeholder="Password"
+                            allowFontScaling={false}
+                            style={[styles.input, { paddingRight: 45 }]}
+                            secureTextEntry={obsecureText}
+                            onFocus={() => {
+                                setFieldTouched('password');
+                            }}
+                            onBlur={() => {
+                                setFieldTouched('password', '');
+                            }}
+                            value={values.password}
+                            onChangeText={handleChange('password')}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                        />
+                        <TouchableOpacity
+                            style={styles.eyeBtn}
+                            onPress={() => {
+                                setObsecureText(!obsecureText);
+                            }}
+                        >
+                            <MaterialCommunityIcons
+                                name={
+                                    obsecureText
+                                        ? 'eye-outline'
+                                        : 'eye-off-outline'
+                                }
+                                size={20}
+                            />
+                        </TouchableOpacity>
+                        {touched.password && errors.password && (
+                            <Text style={styles.errorMessage}>
+                                {errors.password}
+                            </Text>
+                        )}
+                    </View>
+                    <TouchableOpacity
+                        style={styles.forgotBtn}
+                        onPress={openForgotPasswordModal}
+                    >
+                        <Text style={styles.forgotText}>Forgot Password?</Text>
+                    </TouchableOpacity>
+                    <View style={{ marginTop: 20 }}>
+                        {isValid ? (
+                            <TouchableOpacity
+                                onPress={handleSubmit}
+                                style={styles.btnWrapper(
+                                    isValid === false
+                                        ? COLORS.gray
+                                        : COLORS.secondary
+                                )}
+                            >
+                                {loader === false ? (
+                                    <Text style={styles.enterBtnText}>
+                                        S I G N I N
+                                    </Text>
+                                ) : (
+                                    <ActivityIndicator />
+                                )}
+                            </TouchableOpacity>
+                        ) : (
+                            <View
+                                style={styles.btnWrapper(
+                                    isValid === false
+                                        ? COLORS.gray
+                                        : COLORS.primary
+                                )}
+                            >
+                                <Text style={styles.enterBtnText}>
+                                    S I G N I N
+                                </Text>
+                            </View>
+                        )}
+                    </View>
+                </View>
             )}
-          </View>
-
-          <View style={styles.inputWrapper}>
-            <TextInput
-              placeholder="Password"
-              allowFontScaling={false}
-              style={[styles.input, { paddingRight: 45 }]}
-              secureTextEntry={obsecureText}
-              onFocus={() => {
-                setFieldTouched('password');
-              }}
-              onBlur={() => {
-                setFieldTouched('password', '');
-              }}
-              value={values.password}
-              onChangeText={handleChange('password')}
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-            <TouchableOpacity
-              style={styles.eyeBtn}
-              onPress={() => {
-                setObsecureText(!obsecureText);
-              }}
-            >
-              <MaterialCommunityIcons
-                name={obsecureText ? 'eye-outline' : 'eye-off-outline'}
-                size={20}
-              />
-            </TouchableOpacity>
-            {touched.password && errors.password && (
-              <Text style={styles.errorMessage}>{errors.password}</Text>
-            )}
-          </View>
-
-          <View style={{ marginTop: 20 }}>
-            {isValid ? (
-              <TouchableOpacity
-                onPress={handleSubmit}
-                style={styles.btnWrapper(
-                  isValid === false ? COLORS.gray : COLORS.secondary
-                )}
-              >
-                {loader === false ? (
-                  <Text style={styles.enterBtnText}>S I G N I N</Text>
-                ) : (
-                  <ActivityIndicator />
-                )}
-              </TouchableOpacity>
-            ) : (
-              <View
-                style={styles.btnWrapper(
-                  isValid === false ? COLORS.gray : COLORS.primary
-                )}
-              >
-                <Text style={styles.enterBtnText}>S I G N I N</Text>
-              </View>
-            )}
-          </View>
-        </View>
-      )}
-    </Formik>
-  );
+        </Formik>
+    );
 };
 
 export default LoginForm;
 
 const styles = StyleSheet.create({
-  input: {
-    width: '100%',
-    height: 60,
-    backgroundColor: COLORS.gray3,
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-    borderRadius: 8,
-    color: COLORS.primary,
-    fontFamily: 'semiBold',
-    marginBottom: 2,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btnWrapper: (backgroundColor) => ({
-    backgroundColor: backgroundColor,
-    paddingVertical: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-  }),
-  googleBtn: {
-    paddingVertical: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: COLORS.primary,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    gap: 10,
-  },
-  enterBtnText: {
-    fontFamily: 'bold',
-    fontSize: SIZES.medium,
-    color: COLORS.wht,
-  },
-  or: {
-    fontFamily: 'bold',
-    marginVertical: 10,
-    textAlign: 'center',
-  },
-  inputWrapper: {
-    marginBottom: 13,
-  },
-  errorMessage: {
-    color: 'red',
-  },
-  eyeBtn: {
-    position: 'absolute',
-    right: 20,
-    top: 20,
-  },
+    input: {
+        width: '100%',
+        height: 60,
+        backgroundColor: COLORS.gray3,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 8,
+        color: COLORS.primary,
+        fontFamily: 'semiBold',
+        marginBottom: 2,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    forgotText: {
+        fontFamily: 'bold',
+        color: COLORS.primary,
+    },
+    forgotBtn: {
+        marginBottom: 25,
+        marginLeft: 10,
+    },
+    btnWrapper: (backgroundColor) => ({
+        backgroundColor: backgroundColor,
+        paddingVertical: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+    }),
+    googleBtn: {
+        paddingVertical: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        backgroundColor: COLORS.primary,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 10,
+    },
+    enterBtnText: {
+        fontFamily: 'bold',
+        fontSize: SIZES.medium,
+        color: COLORS.wht,
+    },
+    or: {
+        fontFamily: 'bold',
+        marginVertical: 10,
+        textAlign: 'center',
+    },
+    inputWrapper: {
+        marginBottom: 13,
+    },
+    errorMessage: {
+        color: 'red',
+    },
+    eyeBtn: {
+        position: 'absolute',
+        right: 20,
+        top: 20,
+    },
 });
--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,9 +1,12 @@
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    Modal, // Import modal
+    TextInput, // Import text input for forgot password modal
+    Pressable,
+    TouchableWithoutFeedback,
 } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { COLORS, SHADOWS, SIZES } from '../constant/styles';
@@ -11,150 +14,237 @@ import LoginForm from '../components/Auth/LoginForm';
 import RegisterForm from '../components/Auth/RegisterForm';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRoute } from '@react-navigation/native';
+import axios from 'axios';
+import useFetch from '../hook/useFetch';
 
 const AuthScreen = ({ navigation }) => {
-  const [toggle, setToggle] = useState(false);
-  const route = useRoute();
+    const [toggle, setToggle] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false); // Modal visibility state
+    const [email, setEmail] = useState(''); // State for email input
+    const route = useRoute();
+    const { apiUrl } = useFetch();
+    useEffect(() => {
+        if (route.params?.isLogin) {
+            setToggle(true);
+        }
+    }, [route.params]);
 
-  useEffect(() => {
-    if (route.params?.isLogin) {
-      setToggle(true);
-    }
-  }, [route.params]);
+    const handleForgotPassword = async () => {
+        try {
+            const endpoint = `${apiUrl}api/users/forgotPassword`;
+            const response = await axios.post(endpoint, { email });
+            if (response.status === 200) {
+                alert('Password reset link sent to your email.');
+            } else {
+                alert('Error sending password reset email.');
+            }
+        } catch (error) {
+            alert('Failed to send password reset email.');
+        } finally {
+            setModalVisible(false);
+        }
+    };
+    const closeModal = () => {
+        setModalVisible(false);
+    };
 
-  return (
-    <View style={styles.screen}>
-      <View style={styles.container}>
-        <Text style={styles.headerTitle}>Login or create an account</Text>
-        <View style={styles.headerNavBtn}>
-          <TouchableOpacity
-            style={[
-              styles.headerBtnLeft,
-              { backgroundColor: toggle ? COLORS.secondary : COLORS.wht },
-            ]}
-            onPress={() => setToggle(true)}
-          >
-            <Text
-              style={[
-                styles.headerBtnText,
-                {
-                  color: toggle ? COLORS.wht : COLORS.primary,
-                },
-              ]}
-            >
-              Login
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[
-              styles.headerBtnRight,
-              { backgroundColor: toggle ? COLORS.wht : COLORS.secondary },
-            ]}
-            onPress={() => setToggle(false)}
-          >
-            <Text
-              style={[
-                styles.headerBtnText,
-                {
-                  color: toggle ? COLORS.primary : COLORS.wht,
-                },
-              ]}
-            >
-              Register
-            </Text>
-          </TouchableOpacity>
+    return (
+        <View style={styles.screen}>
+            <View style={styles.container}>
+                <Text style={styles.headerTitle}>
+                    Login or create an account
+                </Text>
+                <View style={styles.headerNavBtn}>
+                    <TouchableOpacity
+                        style={[
+                            styles.headerBtnLeft,
+                            {
+                                backgroundColor: toggle
+                                    ? COLORS.secondary
+                                    : COLORS.wht,
+                            },
+                        ]}
+                        onPress={() => setToggle(true)}
+                    >
+                        <Text
+                            style={[
+                                styles.headerBtnText,
+                                {
+                                    color: toggle ? COLORS.wht : COLORS.primary,
+                                },
+                            ]}
+                        >
+                            Login
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.headerBtnRight,
+                            {
+                                backgroundColor: toggle
+                                    ? COLORS.wht
+                                    : COLORS.secondary,
+                            },
+                        ]}
+                        onPress={() => setToggle(false)}
+                    >
+                        <Text
+                            style={[
+                                styles.headerBtnText,
+                                {
+                                    color: toggle ? COLORS.primary : COLORS.wht,
+                                },
+                            ]}
+                        >
+                            Register
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                {toggle ? (
+                    <LoginForm
+                        navigation={navigation}
+                        openForgotPasswordModal={() => setModalVisible(true)}
+                    />
+                ) : (
+                    <RegisterForm navigation={navigation} />
+                )}
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => setModalVisible(!modalVisible)}
+                >
+                    <TouchableWithoutFeedback onPress={closeModal}>
+                        <View style={styles.modalContainer}>
+                            <View style={styles.modalContent}>
+                                <Text style={styles.modalTitle}>
+                                    Forgot Password
+                                </Text>
+                                <TextInput
+                                    placeholder="Enter your email"
+                                    value={email}
+                                    onChangeText={setEmail}
+                                    style={styles.input}
+                                    allowFontScaling={false}
+                                />
+                                <TouchableOpacity
+                                    style={styles.enterBtn}
+                                    onPress={handleForgotPassword}
+                                >
+                                    <Text style={styles.enterBtnText}>
+                                        Submit
+                                    </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={styles.enterBtn}
+                                    onPress={() => setModalVisible(false)}
+                                >
+                                    <Text style={styles.enterBtnText}>
+                                        Close
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </TouchableWithoutFeedback>
+                </Modal>
+            </View>
         </View>
-        {toggle ? (
-          <LoginForm navigation={navigation} />
-        ) : (
-          <RegisterForm navigation={navigation} />
-        )}
-      </View>
-    </View>
-  );
+    );
 };
 
 export default AuthScreen;
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.wht,
-    paddingHorizontal: 40,
-  },
-
-  container: {
-    width: '100%',
-    // alignItems: 'center',
-  },
-  headerTitle: {
-    fontFamily: 'extraBold',
-    textAlign: 'center',
-    fontSize: SIZES.large,
-    color: COLORS.primary,
-  },
-  headerNavBtn: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    marginTop: 20,
-    marginBottom: 40,
-  },
-  headerBtnLeft: {
-    paddingHorizontal: 25,
-    paddingVertical: 8,
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-    ...SHADOWS.medium,
-  },
-  headerBtnRight: {
-    paddingHorizontal: 15,
-    paddingVertical: 8,
-    borderTopRightRadius: 10,
-    borderBottomRightRadius: 10,
-    ...SHADOWS.medium,
-  },
-  headerBtnText: {
-    fontFamily: 'semiBold',
-    fontSize: SIZES.medium,
-  },
-  //!=================
-  input: {
-    width: '100%',
-    height: 60,
-    backgroundColor: COLORS.gray3,
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    borderRadius: 8,
-    color: COLORS.primary,
-    fontFamily: 'semiBold',
-    marginBottom: 15,
-  },
-  enterBtn: {
-    paddingVertical: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: COLORS.secondary,
-  },
-  googleBtn: {
-    paddingVertical: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: COLORS.primary,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    gap: 10,
-  },
-  enterBtnText: {
-    fontFamily: 'bold',
-    fontSize: SIZES.medium,
-    color: COLORS.wht,
-  },
-  or: {
-    fontFamily: 'bold',
-    marginVertical: 10,
-    textAlign: 'center',
-  },
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.wht,
+        paddingHorizontal: 40,
+    },
+    container: {
+        width: '100%',
+    },
+    headerTitle: {
+        fontFamily: 'extraBold',
+        textAlign: 'center',
+        fontSize: SIZES.large,
+        color: COLORS.primary,
+    },
+    headerNavBtn: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginTop: 20,
+        marginBottom: 40,
+    },
+    headerBtnLeft: {
+        paddingHorizontal: 25,
+        paddingVertical: 8,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        ...SHADOWS.medium,
+    },
+    headerBtnRight: {
+        paddingHorizontal: 15,
+        paddingVertical: 8,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        ...SHADOWS.medium,
+    },
+    headerBtnText: {
+        fontFamily: 'semiBold',
+        fontSize: SIZES.medium,
+    },
+    input: {
+        width: '100%',
+        height: 60,
+        backgroundColor: COLORS.gray3,
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 8,
+        color: COLORS.primary,
+        fontFamily: 'semiBold',
+        marginBottom: 15,
+    },
+    enterBtn: {
+        width: '100%',
+        paddingVertical: 6,
+        marginBottom: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        backgroundColor: COLORS.secondary,
+    },
+    enterBtnText: {
+        fontFamily: 'semiBold',
+        fontSize: SIZES.medium,
+        color: COLORS.wht,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        width: '80%',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    modalTitle: {
+        fontSize: 18,
+        marginBottom: 15,
+        fontFamily: 'bold',
+    },
 });
